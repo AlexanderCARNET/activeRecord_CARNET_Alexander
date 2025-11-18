ac99f0062d73f0b7e63b6fd6bdf2e2b869fc357c
Alexander CARNET <e67005u@univ-lorraine.fr>
2025-11-18 11:46:37 +0100

    [UPDATE] Correction du Singleton DBConnection

--- ActiveRecord/src/activeRecord/DBConnection.java
+++ ActiveRecord/src/activeRecord/DBConnection.java
@@ -1,23 +1,64 @@
 package activeRecord;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBConnection {
 
-    private final String userName = "root";
-    private final String password = "";
-    private final String serverName = "localhost";
-    private final String portNumber = "3306";
+    private String password;
+    private String serverName;
+    private String portNumber;
     private String dbName;
     private static DBConnection dbConnection;
+    private Connection connection;
 
     private DBConnection() {
+        //initialisation des attributs pour la connexion a la bd
         this.dbName = "testpersonne";
+        this.password = "";
+        this.serverName = "localhost";
+        this.portNumber = "3306";
+
+        try{
+            //creation de la connection
+            // chargement du driver jdbc
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // creation de la connection
+            Properties connectionProps = new Properties();
+            String userName = "root";
+            connectionProps.put("user", userName);
+            connectionProps.put("password", this.password);
+            String urlDB = "jdbc:mysql://" + this.serverName + ":";
+            urlDB += portNumber + "/" + this.dbName;
+            this.connection = DriverManager.getConnection(urlDB, connectionProps);
+        } catch (SQLException e) {
+            System.out.println("*** ERREUR SQL ***");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("*** ERREUR lors du chargement du driver ***");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("*** ERREUR inconnue... ***");
+            e.printStackTrace();
+        }
+
+
         DBConnection.dbConnection = this;
     }
 
-    public static synchronized DBConnection getConnection(){
+    public static synchronized DBConnection getInstance(){
         if(DBConnection.dbConnection == null){
             return new DBConnection();
         }
         return DBConnection.dbConnection;
     }
+
+    public Connection getConnexion(){
+        return this.connection;
+    }
+
+
 }
