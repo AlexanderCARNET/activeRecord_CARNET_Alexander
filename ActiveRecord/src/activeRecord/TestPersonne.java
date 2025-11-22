81d703b1209e2a896485f4f4d0192933b970b4db
Alexander CARNET <e67005u@univ-lorraine.fr>
2025-11-22 22:54:56 +0100

    [UPDATE] ajout du test pour la methode static findAll de Personne

--- ActiveRecord/src/activeRecord/TestPersonne.java
+++ ActiveRecord/src/activeRecord/TestPersonne.java
@@ -1,7 +1,33 @@
 package activeRecord;
 
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestPersonne {
+
+    @Test
+    public void test_findAll_OK() throws SQLException {
+        Personne p1 = new Personne("Spielberg","Steven");
+        p1.setId(1);
+        Personne p2 = new Personne("Scott","Ridley");
+        p2.setId(2);
+        Personne p3 = new Personne("Kubrick","Stanley");
+        p3.setId(3);
+        Personne p4 = new Personne("Fincher","David");
+        p4.setId(4);
+
+        HashSet<Personne> personnes = Personne.findAll();
+
+        assertEquals(4,personnes.size(), "Pas le bon nombre de personnes");
+
+        assertTrue(personnes.contains(p1));
+        assertTrue(personnes.contains(p2));
+        assertTrue(personnes.contains(p3));
+        assertTrue(personnes.contains(p4));
+    }
 
 }
