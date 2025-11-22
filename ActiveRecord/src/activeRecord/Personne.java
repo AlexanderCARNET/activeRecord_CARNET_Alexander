226d9a3f22b1b80915993405550258685e91a8dd
Alexander CARNET <e67005u@univ-lorraine.fr>
2025-11-22 22:12:48 +0100

    [UPDATE] ajout des methode static pour recuperer toutes les personnes de la bdd et trouver une personne avec son id dans la bdd

--- ActiveRecord/src/activeRecord/Personne.java
+++ ActiveRecord/src/activeRecord/Personne.java
@@ -1,5 +1,11 @@
 package activeRecord;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+
 public class Personne {
     private int id;
     private String nom;
@@ -10,4 +16,53 @@ public class Personne {
         this.nom = nom;
         this.prenom = prenom;
     }
+
+    public static HashSet<Personne> findAll() throws SQLException {
+        HashSet<Personne> personnes = new  HashSet<Personne>();
+
+        //recuperation de la connexion a la bdd
+        Connection connect = DBConnection.getInstance().getConnexion();
+
+        String SQLPrep = "SELECT * FROM Personne;";
+        PreparedStatement prep1 = connect.prepareStatement(SQLPrep);
+        prep1.execute();
+        ResultSet rs = prep1.getResultSet();
+        // s'il y a un resultat
+        while (rs.next()) {
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+            int id = rs.getInt("id");
+            Personne p = new Personne(nom,prenom);
+            p.setId(id);
+            personnes.add(p);
+        }
+        return personnes;
+    }
+
+    public static Personne findById(int id) throws SQLException {
+        Personne res = null;
+
+        //recuperation de la connexion a la bdd
+        Connection connect = DBConnection.getInstance().getConnexion();
+
+        String SQLPrep = "SELECT * FROM Personne WHERE id=?;";
+        PreparedStatement prep1 = connect.prepareStatement(SQLPrep);
+        prep1.setInt(1, id);
+        prep1.execute();
+        ResultSet rs = prep1.getResultSet();
+        // s'il y a un resultat
+        if (rs.next()) {
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+
+            res = new Personne(nom,prenom);
+            res.setId(id);
+        }
+        return res;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
 }
